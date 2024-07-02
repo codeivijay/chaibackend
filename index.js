7aff03e93ce1516d24707dbcbd43d4ee304b33eb
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-07-02 16:06:22 +0530

    github data

--- index.js
+++ index.js
@@ -3,6 +3,43 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+const githubData = {
+    "login": "codeivijay",
+    "id": 105920990,
+    "node_id": "U_kgDOBlA53g",
+    "avatar_url": "https://avatars.githubusercontent.com/u/105920990?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/codeivijay",
+    "html_url": "https://github.com/codeivijay",
+    "followers_url": "https://api.github.com/users/codeivijay/followers",
+    "following_url": "https://api.github.com/users/codeivijay/following{/other_user}",
+    "gists_url": "https://api.github.com/users/codeivijay/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/codeivijay/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/codeivijay/subscriptions",
+    "organizations_url": "https://api.github.com/users/codeivijay/orgs",
+    "repos_url": "https://api.github.com/users/codeivijay/repos",
+    "events_url": "https://api.github.com/users/codeivijay/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/codeivijay/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Vijay Singh Diggi",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": "codeivijay",
+    "public_repos": 11,
+    "public_gists": 0,
+    "followers": 1,
+    "following": 8,
+    "created_at": "2022-05-20T10:53:05Z",
+    "updated_at": "2024-05-21T13:08:20Z"
+}
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -13,6 +50,10 @@ app.get('/twitter', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.send("<h1> Please Login at chai aur code </h1>")
+})
+
+app.get('/github',(req, res) => {
+    res.json(githubData)
 })
 
 app.get('/youtube', (req, res) => {
